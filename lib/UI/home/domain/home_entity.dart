0fccc3afba54da4da6f620f46dc01259a4bab27f
山崎謙登 <chihayayome@yahoo.co.jp>
2023-10-02 22:20:47 +0900

    feat: Added files for home screen UI

---
+++ lib/UI/home/domain/home_entity.dart
@@ -0,0 +1,62 @@
+class HomeEntity {
+  int id;
+  String name;
+  DateTime? released;
+  bool tba;
+  Uri? backgroundImage;
+  int? rating;
+  int? ratingTop;
+  List<HomeRatingEntity>? ratings;
+  int? ratingsCount;
+  int? metacritic;
+  List<HomePlatformEntity>? platforms;
+  List<HomeGenreEntity>? genres;
+  List<HomeTagEntity>? tags;
+  List<Uri>? shortScreenshots;
+
+  HomeEntity(
+      this.id,
+      this.name,
+      this.released,
+      this.tba,
+      this.backgroundImage,
+      this.rating,
+      this.ratingTop,
+      this.ratings,
+      this.ratingsCount,
+      this.metacritic,
+      this.platforms,
+      this.genres,
+      this.tags,
+      this.shortScreenshots);
+}
+
+class HomeRatingEntity {
+  int id;
+  String title;
+  int count;
+  double percent;
+
+  HomeRatingEntity(this.id, this.title, this.count, this.percent);
+}
+
+class HomePlatformEntity {
+  int id;
+  String title;
+
+  HomePlatformEntity(this.id, this.title);
+}
+
+class HomeGenreEntity {
+  int id;
+  String name;
+
+  HomeGenreEntity(this.id, this.name);
+}
+
+class HomeTagEntity {
+  int id;
+  String name;
+
+  HomeTagEntity(this.id, this.name);
+}
